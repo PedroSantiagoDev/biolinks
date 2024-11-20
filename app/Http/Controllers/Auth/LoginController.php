a39d807a672f6278ea379014b9c4970528f66148
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-11-20 14:31:52 -0300

    feat: login done

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\MakeLoginRequest;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\View\View;
+
+class LoginController extends Controller
+{
+    public function index(): View
+    {
+        return view('auth.login');
+    }
+
+    public function login(MakeLoginRequest $request): RedirectResponse
+    {
+        if ($request->attempt()) {
+            return to_route('dashboard');
+        }
+
+        // Returns to the page it called and saves a message in the session
+        return back()->with(['message' => 'não encontrado']);
+    }
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,27 @@
+<div>
+    <h1>Login</h1>
+
+    @if($message = session()->get('message'))
+    <span>{{$message}}</span>
+    @endif
+
+    <form action="/login" method="post">
+        @csrf
+
+        <div>
+            <input type="text" name="email" placeholder="Email" value="{{ old('email') }}" />
+            @error('email')
+            <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <div>
+            <input type="password" name="password" placeholder="Senha" />
+            @error('password')
+            <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <button>Logar</button>
+    </form>
+</div>
