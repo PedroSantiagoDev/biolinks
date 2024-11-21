4d8331bc34e6a0a661f7fbc80a882fe431e06932
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-11-21 19:41:46 -0300

    feat: remove class

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\MakeLoginRequest;
-use App\Models\User;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
 class LoginController extends Controller
