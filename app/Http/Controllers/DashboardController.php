7d533cda9214369543987c4caaabb47816d10eb2
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-11-24 17:25:14 -0300

    feat: seeder e factory

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,6 +12,8 @@ class DashboardController extends Controller
         /** @var User $user */
         $user = auth()->user();
 
-        return view('dashboard');
+        return view('dashboard', [
+            'links' => $user->links
+        ]);
     }
 }
