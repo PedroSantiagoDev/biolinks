f9ea0191df0ebe9d9b1052ddebea054a4114b6e6
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-11-26 16:20:27 -0300

    feat: add sort

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,21 +2,38 @@
     <h1>Dashboard</h1>
 
     @if($message = session()->get('message'))
-        <span>{{$message}}</span>
+    <span>{{$message}}</span>
     @endif
 
     <a href="{{ route('links.create') }}">Criar</a>
 
     <ul>
         @foreach ($links as $link)
-            <li>
-                <a href="{{ route('links.edit', $link) }}">{{ $link->name }}</a>
-                <form action="{{ route('links.destroy', $link) }}" method="post" onsubmit="return confirm('Tem certeza?')">
-                    @csrf
-                    @method('delete')
-                    <button>deletar</button>
-                </form>
-            </li>
+        <li style="display: flex;">
+            @unless ($loop->last)
+            <form action="{{ route('links.down', $link) }}" method="post">
+                @csrf
+                @method('patch')
+                <button>↓</button>
+            </form>
+            @endunless
+
+            <a href="{{ route('links.edit', $link) }}">{{ $link->name }}</a>
+
+            @unless($loop->first)
+            <form action="{{ route('links.up', $link) }}" method="post">
+                @csrf
+                @method('patch')
+                <button>↑</button>
+            </form>
+            @endunless
+
+            <form action="{{ route('links.destroy', $link) }}" method="post" onsubmit="return confirm('Tem certeza?')">
+                @csrf
+                @method('delete')
+                <button>deletar</button>
+            </form>
+        </li>
         @endforeach
     </ul>
 </div>
