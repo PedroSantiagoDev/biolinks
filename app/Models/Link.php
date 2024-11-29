1199d05448dcc14be26c83ca2fe1b79557d88c18
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-11-29 17:06:49 -0300

    feat: up and move

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -13,5 +13,40 @@ class Link extends Model
     protected $fillable = [
         'link',
         'name',
+        'sort',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function moveUp()
+    {
+        $this->move(-1);
+    }
+
+    public function moveDown()
+    {
+        $this->move(1);
+    }
+
+    /**
+     * Function responsible for reader the link
+     * 
+     * @param int $to +1 for moving down and -1 for moving up
+     * @return void
+     */
+    private function move(int $to)
+    {
+        $order = $this->sort;
+        $newOrder = $order + $to;
+
+        $user = auth()->user();
+
+        $swapWith = $user->links()->where('sort', '=', $newOrder)->first();
+
+        $this->fill(['sort' => $newOrder])->save();
+        $swapWith->fill(['sort' => $order])->save();
+    }
 }
