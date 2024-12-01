c6ec3e18b48a400a2ea857c49b6996ecf1bf07a9
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-12-01 16:49:00 -0300

    feat: add checkHandler

--- app/Http/Requests/ProfileRequest.php
+++ app/Http/Requests/ProfileRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\CheckHandler;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -27,7 +28,8 @@ class ProfileRequest extends FormRequest
             'description' => ['nullable'],
             'handler' => [
                 'required',
-                Rule::unique('users')->ignoreModel($this->user())
+                Rule::unique('users')->ignoreModel($this->user()),
+                new CheckHandler
             ]
         ];
     }
