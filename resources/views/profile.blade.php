7473dcc3e23476ee6b4de5e3389dd05c8236e518
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-12-02 11:01:01 -0300

    feat: add photo

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,9 +3,15 @@
     @if ($message = session('message'))
         <div>{{ $message }}</div>
     @endif
-    <form action="{{ route('profile') }}" method="post">
+    <form action="{{ route('profile') }}" method="post" enctype="multipart/form-data">
         @csrf
         @method('put')
+
+        <div>
+            <img src="/storage/{{ $user->photo }}" alt="Profile Picture">
+            <input type="file" name="photo">
+        </div>
+
         <div>
             <input name="name" placeholder="Nome" value="{{ old('name', $user->name) }}" />
             @error('name')
